94303250c3e70aa94a676c48bd1c88f2481197db
megfly <mflynn333@gmail.com>
2020-10-21 16:14:04 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+
+function Scooter(year, color, model){
+    this.year = year;
+    this.color = color;
+    this.model = model
+}
+
+function Driver(name, age, experience){
+    this.name = name;
+    this.age = age;
+    this.experience = experience;
+}
+
+function PickupLocation(address, city){
+    this.address = address;
+    this.city = city;
+}
+
+
+// let lauren = new User("Lauren", "lauren@example.com");
+// lauren.name //=> "Lauren"
